86624da0245ad23d3ef34c7b5bfdf7dd842e7141
Gustavo A <124809454+GustavoAGV@users.noreply.github.com>
2023-06-15 00:27:25 -0600

    Delete aaaaaaaaaaaaaaaaaaaaaaaaaa directory

--- aaaaaaaaaaaaaaaaaaaaaaaaaa/JS/app1.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function()
-{
-	$(".black").on("click",function()
-	{
-		var nombre=$(".input").val();
-		var comentar=$(".inpu").val();
-		$(".comentario").html("de:"+nombre+"<br>"+"comentario:"+comentar);
-	})
-})
